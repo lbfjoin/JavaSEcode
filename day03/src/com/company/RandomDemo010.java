a823dfed0669c7e8a79952e15b2146a309f2d6a7
lbfjoin <1584215983@qq.com>
2020-04-21 16:39:43 +0800

    java

--- day03/src/com/company/RandomDemo010.java
+++ day03/src/com/company/RandomDemo010.java
@@ -6,6 +6,12 @@ package com.company;
  */
 public class RandomDemo010 {
     public static void main(String[] args) {
+//        github 链接
+//        简书 链接
+//        已完成
+//        内容： 1.将前两次的基础部分代码push 到git 中，day01;day 02
+//              2.整理今天内容（简书）
+//              3.idea破解
 
     }
 }
