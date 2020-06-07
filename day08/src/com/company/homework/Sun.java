cb15a5201e4aca97569098dd58f4b9dbbdc562fc
lbfjoin <1584215983@qq.com>
2020-06-07 19:45:33 +0800

    000

--- day08/src/com/company/homework/Sun.java
+++ day08/src/com/company/homework/Sun.java
@@ -9,7 +9,10 @@ public class Sun extends Star implements Universe {
     @Override
     public void doAnything() {
         System.out.println("sun:太阳吸引着9大行星旋转");
-        System.out.println("sun:光照八分钟，到达地球");
     }
 
+    @Override
+    public void shine() {
+        System.out.println("sun:光照八分钟，到达地球");
+    }
 }
--- day08/src/com/company/homework/Test.java
+++ day08/src/com/company/homework/Test.java
@@ -8,8 +8,11 @@ public class Test {
     public static void main(String[] args) {
         Star star = new Star();
         star.shine();
-
+        System.out.println("==================");
         Universe sun = new Sun();
         sun.doAnything();
+
+        Sun sun1 = (Sun)sun;
+        sun1.shine();
     }
 }
