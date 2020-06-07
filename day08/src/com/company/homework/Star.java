3429bc5d7ac9b2db65f1c32adbbaa44e65c0aa20
lbfjoin <1584215983@qq.com>
2020-06-07 19:46:21 +0800

    000

--- day08/src/com/company/homework/Star.java
+++ day08/src/com/company/homework/Star.java
@@ -8,10 +8,4 @@ public class Star {
     public void shine(){
         System.out.println("star:星星一闪一闪亮晶晶");
     }
-    public void getUniverse(Universe universe){
-        if (universe != null){
-            if (universe instanceof Sun)
-                universe.doAnything();
-        }
-    }
 }
