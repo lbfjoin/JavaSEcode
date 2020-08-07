3cc3e65f0ea8d2b7e24d2e8b6d11cd636749e91b
lbfjoin <1584215983@qq.com>
2020-08-07 10:15:30 +0800

    111

--- Fy_Java9_JavaSE/src/jdbc/StudentAdmin/MainStudent.java
+++ Fy_Java9_JavaSE/src/jdbc/StudentAdmin/MainStudent.java
@@ -21,30 +21,28 @@ public class MainStudent {
         for (Student s : list) {
             System.out.println(s);
         }
-//        System.out.println("--------保存学生------------");
-//        // new Student
-//        //studentDao.save();
-//        Student student = new Student(2,"李四","男",66,"沈阳","55555555");
-//        StudentDaoImpl studentDao1 = new StudentDaoImpl();
-//        studentDao.save(student);
-//        List<Student> list1 = studentDao1.findAll();
-//        for (Student s:list1){
-//            System.out.println(s);
-//        }
+        System.out.println("--------保存学生------------");
+        // new Student
+        //studentDao.save();
+        Student student = new Student(2,"李四","男",66,"沈阳","55555555");
+        studentDao.save(student);
+        List<Student> list1 = studentDao.findAll();
+        for (Student s:list1){
+            System.out.println(s);
+        }
         System.out.println("----------删除学生----------");
         //删除
-        StudentDaoImpl studentDao2 = new StudentDaoImpl();
-        studentDao2.remove(2);
-        List<Student> students = studentDao2.findAll();
+
+        studentDao.remove(2);
+        List<Student> students = studentDao.findAll();
         for (Student s : students) {
             System.out.println(s);
         }
 
         //修改
         System.out.println("----------修改学生----------");
-        StudentDaoImpl studentDao3 = new StudentDaoImpl();
         Student student1 = new Student(1, "张三", "男", 90, "锦州", "666666666");
-        studentDao3.update(student1);
+        studentDao.update(student1);
         List<Student> list3 = studentDao.findAll();
         for (Student s : list3) {
             System.out.println(s);
